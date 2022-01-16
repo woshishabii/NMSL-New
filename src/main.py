1947470a4df61111e4243f50dee8613505d27d5f
星辰 <jgbsxx20130315@163.com>
2022-01-16 21:54:07 +0800

    Install Package&Create Main Packages

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+alpha3
+NMSL - New
+"""
+
+import settings.Settings
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    SLSettings = settings.Settings.Base()
+    main()
---
+++ src/settings/Settings.py
@@ -0,0 +1,8 @@
+class Base:
+    def __init__(self):
+        self.name = 'Naughty Minecraft Server Launcher'
+        self.version = [
+            ['alpha', 3, 1],
+            'Alpha3-1'
+        ]
+        self.author = 'woshishabi'
