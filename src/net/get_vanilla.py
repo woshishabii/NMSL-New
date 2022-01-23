ee5b955fd78a8ecfd560e59b0b78991ee1b7d0f6
星辰 <jgbsxx20130315@163.com>
2022-01-23 18:33:35 +0800

    finish get mojang data

--- src/net/get_vanilla.py
+++ src/net/get_vanilla.py
@@ -7,13 +7,25 @@ by:woshishabi
 # 导入库
 import requests
 import json
+import sys
 
 
 # 获取mojang服务器数据
 def get_by_mojang(sl_settings):
     manifest_request = requests.get(sl_settings.sources['mojang'])
+    # Original Data / 原始数据
     o_data = json.loads(manifest_request.text)
     # sl_log.log(message=data)
     p_data = {}
     # p_data['latest'] = o_data['latest']
+    p_data['release'] = {}
+    p_data['snapshot'] = {}
+    p_data['old_beta'] = {}
+    p_data['old_alpha'] = {}
+
+    for temp in o_data['versions']:
+        # print(temp)
+        p_data[temp['type']][temp['id']] = [temp['url'], temp['time']]
+
+    p_data['latest'] = o_data['latest']
     return p_data
